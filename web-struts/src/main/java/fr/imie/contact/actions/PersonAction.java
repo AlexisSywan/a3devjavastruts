656ef6af70319f7bae459de3fa2df2cec7b2096b
Thomas Rouault <throuault44@gmail.com>
2019-05-15 10:26:03 +0200

    Mise à jour PersonAction.java

--- web-struts/src/main/java/fr/imie/contact/actions/PersonAction.java
+++ web-struts/src/main/java/fr/imie/contact/actions/PersonAction.java
@@ -32,58 +32,18 @@ public class PersonAction extends ActionSupport {
     private Person person;
 
     public String save() {
-        System.out.println(person.getFirstName());
+//        repository.save();
         return SUCCESS;
     }
     public String deleteById() {
+//        repository.deleteById(id);
         return SUCCESS;
     }
     public String findAll() {
-     //   List<Person> persons = repository.findAll();
-       // request.setAttribute("persons", persons);
-       // request.getRequestDispatcher();
-        return SUCCESS;
-    }
-
-//    protected void service(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//
-//        Person person = new Person();
-//
-//        if (request.getMethod().equalsIgnoreCase("post")){
-//
-//            if(request.getParameter("id") != null) {
-//                person =repository.findById(Integer.parseInt(request.getParameter("id")));
-//            }
-//
-//            person.setFirstName(request.getParameter("firstName"));
-//            person.setLastName(request.getParameter("lastName"));
-//            person.setEmail(request.getParameter("email"));
-//
-//            String text = request.getParameter("birthDate");
-//            LocalDate date = DateUtils.toLocalDate(text);
-//            person.setBirthDate(date);
-//
-//
-//            repository.save(person);
-//        }
-//
-//        if(request.getPathInfo() != null && request.getPathInfo().matches("/delete/[0-9]+")) {
-//
-//            Integer id = Integer.parseInt(request.getPathInfo().split("/")[2]);
-//            repository.deleteById(id);
-//
-//        }
-//
-//        if(request.getPathInfo() != null && request.getPathInfo().matches("/edit/[0-9]+")) {
-//
-//            Integer id = Integer.parseInt(request.getPathInfo().split("/")[2]);
-//            request.setAttribute("id", id);
-//        }
-//
 //        List<Person> persons = repository.findAll();
 //        request.setAttribute("persons", persons);
 //        request.getRequestDispatcher("/WEB-INF/views/person.jsp").forward(request, response);
-//    }
+        return SUCCESS;
+    }
 
 }
