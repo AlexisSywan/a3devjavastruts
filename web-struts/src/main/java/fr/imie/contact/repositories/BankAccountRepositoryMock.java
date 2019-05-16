33ddf320e9dc6ea45bea8930cc560b8de807931c
Thomas Rouault <throuault44@gmail.com>
2019-05-16 12:24:41 +0200

    Up de struts

--- web-struts/src/main/java/fr/imie/contact/repositories/BankAccountRepositoryMock.java
+++ web-struts/src/main/java/fr/imie/contact/repositories/BankAccountRepositoryMock.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collector;
 
 public class BankAccountRepositoryMock implements BankAccountRepository {
 
